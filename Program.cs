5df1e9dfd6ae08a5a029a4e002efdd0b49f99dcd
kiki <mullerkrisztianbusiness@gmail.com>
2024-10-02 15:27:06 +0200

    Api/Skeleton

--- Program.cs
+++ Program.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
+using FourBits.Backend;
 
 
 namespace FourBits {
 
+    /// <summary>
+    /// Akkor van, ha olyan endpointot kértek, amilyen nincs.
+    /// </summary>
+    class NotFoundException : Exception {
+        // semmi
+    }
+
     class Program {
 
         static readonly int PORT = 8084;
+        static readonly string WWW_DIR = "www";
+
+        static byte[] EncodeString(string str) => System.Text.Encoding.UTF8.GetBytes(str);
+
+
+        static Conversation conversation = new Conversation();
+
 
         static void Main(string[] args) {
+            conversation.InsertMessage(new Message("Béla", "Szia"));
+            Thread.Sleep(2000);
+            conversation.InsertMessage(new Message("Béla 2.0", "Üdv"));
+
+            Console.WriteLine(conversation.ToJson().ToJsonString());
+
             var listenTask = Task.Run( () => ListenTask(CancellationToken.None) );
             listenTask.Wait();
         }
@@ -23,23 +46,97 @@ namespace FourBits {
                 Console.WriteLine($"{nameof(HttpListener)} now listening");
                 while(true) {
                     HttpListenerContext context = await listener.GetContextAsync();
-                    Console.WriteLine($"Handling {context.Request.RemoteEndPoint}");
-                    await HandleRequest(context);
+                    try {
+                        Console.WriteLine($"Handling {context.Request.RemoteEndPoint}");
+                        await HandleRequest(context);
+                    } catch(Exception e) {
+                        Console.WriteLine($"Exception while handling request: {e}");
+                        try {
+                            context.Response.StatusCode = 500;
+                        } finally {
+                            context.Response.Close();
+                        }
+                    }
                 }
                 listener.Stop();
             }
         }
 
         static async Task HandleRequest(HttpListenerContext context) {
-            string responseString = "<html><body> Hello world!</body></html>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            string path = System.Web.HttpUtility.UrlDecode(context.Request.Url?.AbsolutePath ?? "/");
+            if(path.Contains("..")) context.Response.Abort(); // Szűrjük ki a gonosz path traversaleket
 
-            HttpListenerResponse response = context.Response;
-            response.ContentLength64 = buffer.Length;
+            Console.WriteLine($"{context.Request.RemoteEndPoint} -{context.Request.HttpMethod}-> {path}");
 
-            System.IO.Stream output = response.OutputStream;
-            await output.WriteAsync(buffer, 0, buffer.Length);
-            output.Close();
+            if(path == "/") {
+                await SendFile(context.Response, $"{WWW_DIR}/index.html");
+            } else if(File.Exists($"{WWW_DIR}{path}")) {
+                await SendFile(context.Response, $"{WWW_DIR}{path}");
+            } else {
+                await ApiCall(context, path);
+            }
+
+            context.Response.Close();
+        }
+
+
+        static async Task SendFile(HttpListenerResponse response, string path) {
+
+            // TODO streamreaderrel beolvasni a fájlt és azt írni a bufferbe (próbáld asynccel)
+            byte[] buffer = EncodeString("<html><body> Hello world!</body></html>");
+
+            await response.OutputStream.WriteAsync(buffer);
+            response.OutputStream.Close();
+        }
+
+
+        static async Task ApiCall(HttpListenerContext context, string path) {
+            try {
+                switch(context.Request.HttpMethod) {
+                    case "GET":
+                        JsonNode result = ApiGet(path, context.Request.QueryString);
+                        await context.Response.OutputStream.WriteAsync(
+                            EncodeString(result.ToJsonString())
+                        );
+                        context.Response.StatusCode = 200;
+                        break;
+
+                    case "POST":
+                        JsonNode? payload = await JsonNode.ParseAsync(context.Request.InputStream);
+
+                        ApiPost(path, context.Request.QueryString, payload);
+
+                        context.Response.StatusCode = 200;
+                        break;
+
+                    default:
+                        context.Response.StatusCode = 405;
+                        break;
+                }
+            } catch(NotFoundException) {
+                await context.Response.OutputStream.WriteAsync(EncodeString("Not found"));
+                context.Response.StatusCode = 404;
+            }
+        }
+
+        static JsonNode ApiGet(string path, System.Collections.Specialized.NameValueCollection query) {
+            switch(path) {
+                case "/messages":
+                    return JsonValue.Create(149); // TODO
+            }
+
+            throw new NotFoundException();
+        }
+
+        static void ApiPost(string path, System.Collections.Specialized.NameValueCollection query, JsonNode? json) {
+            switch(path) {
+                case "/message":
+                    // TODO
+                    Console.WriteLine(json?.ToJsonString() ?? "null");
+                    return;
+            }
+
+            throw new NotFoundException();
         }
 
     }
